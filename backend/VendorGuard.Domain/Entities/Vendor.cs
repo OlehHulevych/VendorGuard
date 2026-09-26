14c8a92136dda31edb877c48ffa1172b5d6f47bc
Oleh_Hulevych <olegguelvic8@gmail.com>
2026-09-26 22:17:44 +0200

    added vendor entity

---
+++ backend/VendorGuard.Domain/Entities/Vendor.cs
@@ -0,0 +1,17 @@
+﻿using VendorGuard.Domain.Common;
+
+namespace VendorGuard.Domain.Entities;
+
+public class Vendor:BaseEntity
+{
+    public string LegalName { get; }
+    public string? Lei { get; }
+    public string? CountryCode { get; }
+    public string? CompanydId { get; }
+    public string? Website { get; }
+    public string? ContactName { get; }
+    public string? ContactEmail { get; }
+    public bool IsIntraGroup { get; }
+    
+    
+}
---
+++ backend/VendorGuard.Domain/Enums/VendorStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace VendorGuard.Domain.Enums;
+
+public enum VendorStatus
+{
+    Onboarding = 1,
+    Active = 2,
+    Offboarded = 3
+}
